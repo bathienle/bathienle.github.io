4b26ac1240a045eb7e759785adecb92bab005379
Ba Thien LE <bathien.le.pro@gmail.com>
2022-02-23 19:30:48 +0100

    Add contact information

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,5 +1,18 @@
 <template>
   <div class="contact">
-    <h1>Contact</h1>
+    <h1 class="title">Contact</h1>
+    <div class="block">
+      <a href="mailto:ba.le@uliege.be">ba.le@uliege.be</a>
+    </div>
+    <div class="block">
+      Montefiore Institute (B28)<br />
+      Quartier Polytech 1<br />
+      Allée de la découverte 10<br />
+      4000 Liège<br />
+    </div>
+    <div class="block">
+      Office I.128
+    </div>
+    <div></div>
   </div>
 </template>
