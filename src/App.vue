9c21392822c40065c58bac6de9126952934c950a
Ba Thien LE <bathien.le.pro@gmail.com>
2022-06-12 01:10:15 +0200

    Fetch data from cosmicjs

--- src/App.vue
+++ src/App.vue
@@ -19,6 +19,8 @@ import Experience from "@/views/Experience.vue";
 import Skill from "@/views/Skill.vue";
 import Project from "@/views/Project.vue";
 
+import { bucket } from "./cosmic.js";
+
 export default {
   name: "App",
   components: {
@@ -29,6 +31,48 @@ export default {
     Skill,
     Project,
     Footing,
+  },
+  data: () => ({
+    user: {},
+    experiences: {},
+    skills: {},
+    projects: {},
+  }),
+  methods: {
+    fetchData() {
+      return new Promise((resolve) => {
+        resolve(bucket.getObjects({ props: "title,type,content,metadata" }));
+      });
+    },
+    getData(type, objects) {
+      return objects.filter((item) => item.type === type);
+    },
+    fillUserInfo(userInfo) {
+      let metadata = userInfo.metadata;
+      this.user = {
+        name: metadata.name,
+        job_title: metadata.job_title,
+        office: metadata.office,
+        work_address: metadata.work_address,
+        github: metadata.github,
+        linkedin: metadata.linkedin,
+      };
+    },
+    fillExperiences(experiences) {
+      this.experiences = {
+        education: experiences.filter(
+          (item) => item.metadata.type === "education"
+        ),
+        work: experiences.filter((item) => item.metadata.type === "work"),
+      };
+    },
+  },
+  created() {
+    this.fetchData().then((values) => {
+      let objects = values.objects;
+      this.fillUserInfo(this.getData("user", objects).pop());
+      this.fillExperiences(this.getData("experiences", objects));
+    });
   },
 };
 </script>
