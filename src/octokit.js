fbdfbd827d4e48bff1dd207ffbba3e2b6c62a55b
Restyled.io <commits@restyled.io>
2022-06-17 23:16:49 +0000

    Restyled by prettier

--- src/octokit.js
+++ src/octokit.js
@@ -1,5 +1,5 @@
 import { Octokit } from "octokit";
 
 export const octokit = new Octokit({
-    auth: "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"
+  auth: "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",
 });
