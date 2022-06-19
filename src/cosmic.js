e63b7c82a0fff5023eabcacfc60495ea6fdb1d6a
Ba Thien LE <bathien.le.pro@gmail.com>
2022-06-19 13:52:29 +0200

    Generate new keys for CosmisJS and otokit.js

--- src/cosmic.js
+++ src/cosmic.js
@@ -2,6 +2,6 @@ import Cosmic from "cosmicjs";
 
 const cosmic = Cosmic();
 export const bucket = cosmic.bucket({
-  slug: "bf7f2230-e1d0-11ec-960c-fdfbed0f67d7",
-  read_key: "QEiPA0YOlTDr1LzbChUVhljC2vP81SZQ7cOVvho2bgJ93F2W1s",
+  slug: process.env.VUE_APP_COSMICJS_SLUG,
+  read_key: process.env.VUE_APP_COSMICJS_TOKEN,
 });
