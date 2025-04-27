09d5f83ba39f3247a8c506d25b2b29477d3b2ae2
Ba Thien Le <bathien.le.pro@gmail.com>
2025-04-27 19:52:37 +0200

    Add Tailwind CSS

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
@@ -14,6 +15,7 @@ export default defineConfig({
     port: 5173,
   },
   plugins: [
+    tailwindcss(),
     vue(),
     vueDevTools(),
   ],
