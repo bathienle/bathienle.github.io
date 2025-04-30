2a405a376e8e378fc496f832f9c737cd411efac2
Ba Thien Le <bathien.le.pro@gmail.com>
2025-04-30 22:55:46 +0200

    Make navbar stick to the top

--- src/components/AppNavbar.vue
+++ src/components/AppNavbar.vue
@@ -1,14 +1,14 @@
 <template>
-  <header class="bg-black text-white shadow-md">
+  <header class="sticky top-0 z-50 bg-black text-white shadow-md">
     <div class="mx-auto flex items-center justify-between p-4">
       <div v-if="profile" class="text-xl font-bold">
         {{ profile.firstName }} {{ profile.lastName }}
       </div>
 
-      <nav class="flex ">
+      <nav class="flex">
         <a href="#home" class="p-2 hover:bg-green-700 rounded-full">Home</a>
         <a href="#about" class="p-2 hover:bg-green-700 rounded-full">About</a>
-        <a href="#skills" class="p-2 hover:bg-green-700 rounded-full">Skills</a>
+        <a href="#education" class="p-2 hover:bg-green-700 rounded-full">Education</a>
         <a href="#experiences" class="p-2 hover:bg-green-700 rounded-full">Experiences</a>
         <a href="#projects" class="p-2 hover:bg-green-700 rounded-full">Projects</a>
       </nav>
