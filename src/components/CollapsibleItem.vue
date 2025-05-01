e2d5d7ffc3946c77b1e4894f9dd3d36a4984ac91
Ba Thien Le <bathien.le.pro@gmail.com>
2025-05-01 16:27:02 +0200

    Add icon for collapse

--- src/components/CollapsibleItem.vue
+++ src/components/CollapsibleItem.vue
@@ -2,8 +2,14 @@
   <div class="w-full max-w-md mx-auto mt-4 p-2">
     <button 
       @click="isOpen = !isOpen"
-      class="w-full px-4 py-2 text-left bg-gray-800 text-white rounded-lg shadow hover:bg-blue-700 transition">
+      class="flex items-center justify-between px-4 py-2 text-left bg-gray-800 text-white rounded-lg shadow hover:bg-blue-700 transition"
+    >
       <slot name="heading"></slot>
+
+      <span>
+        <BarsArrowUpIcon v-if="isOpen" class="size-6" />
+        <BarsArrowDownIcon v-else class="size-6" />
+      </span>
     </button>
 
     <div v-show="isOpen" class="p-2 rounded-lg shadow transition-all duration-300">
@@ -14,6 +20,7 @@
 
 <script setup lang="ts">
 import { ref } from 'vue';
+import { BarsArrowDownIcon, BarsArrowUpIcon } from '@heroicons/vue/24/solid';
 
 const isOpen = ref(false);
 </script>
