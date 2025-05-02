20d9dc59a56bbf0ccc5fc7b99a8b05d5ac8ef0e5
Ba Thien Le <bathien.le.pro@gmail.com>
2025-05-02 20:15:58 +0200

    Justify about text

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -2,7 +2,7 @@
   <section id="about" class="scroll-mt-20">
     <h1 class="text-3xl text-gray-300 font-bold p-1.5">About</h1>
 
-    <p v-if="about" class="p-1.5">
+    <p v-if="about" class="text-justify p-1.5">
       {{ about.description }}
     </p>
   </section>
