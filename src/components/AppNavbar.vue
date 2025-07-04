12879735c082f91f544960faac26a710ae30764e
Ba Thien Le <bathien.le.pro@gmail.com>
2025-07-04 22:42:50 +0200

    Center navbar items

--- src/components/AppNavbar.vue
+++ src/components/AppNavbar.vue
@@ -1,18 +1,36 @@
 <template>
   <header class="sticky top-0 z-50 bg-black text-white shadow-md">
-    <div class="mx-auto flex items-center justify-between p-4">
-      <div v-if="profile" class="text-xl font-bold">
-        <a class="hover:text-blue-500" :href="profile.website" rel="noopener">
-          {{ profile.firstName }} <span class="uppercase">{{ profile.lastName }}</span>
-        </a>
-      </div>
+    <div class="flex items-center justify-center">
+      <div class="mx-auto max-w-7xl px-4">
+        <div class="grid grid-cols-3 items-center h-16">
 
-      <nav class="flex">
-        <a href="#about" class="p-2 hover:bg-blue-500 rounded-full">About</a>
-        <a href="#experience" class="p-2 hover:bg-blue-500 rounded-full">Experience</a>
-        <a href="#education" class="p-2 hover:bg-blue-500 rounded-full">Education</a>
-        <a href="#project" class="p-2 hover:bg-blue-500 rounded-full">Projects</a>
-      </nav>
+          <div v-if="profile" class="text-xl font-bold justify-start">
+            <a class="hover:text-blue-500 transition-colors duration-200" :href="profile.website" rel="noopener">
+              {{ profile.firstName }} <span class="uppercase">{{ profile.lastName }}</span>
+            </a>
+          </div>
+
+          <nav class="flex items-center justify-center">
+            <a href="#about" class="px-3 py-2 rounded-md hover:bg-blue-500 duration-200">About</a>
+            <a href="#experience" class="px-3 py-2 rounded-md hover:bg-blue-500 duration-200">Experience</a>
+            <a href="#education" class="px-3 py-2 rounded-md hover:bg-blue-500 duration-200">Education</a>
+            <a href="#project" class="px-3 py-2 rounded-md hover:bg-blue-500 duration-200">Projects</a>
+          </nav>
+
+          <div class="flex items-center justify-end gap-2">
+            <a
+              v-for="social in socials"
+              :key="social.key"
+              :href="social.url"
+              rel="noopener"
+              target="_blank"
+              class="hover:text-blue-500 transition-colors duration-200"
+            >
+              <i :class="`fab fa-${social.key} text-lg`" />
+            </a>
+          </div>
+        </div>
+      </div>
     </div>
   </header>
 </template>
@@ -25,4 +43,5 @@ import type { Content } from '@/types/content.d.ts';
 
 const content = inject<Ref<Content>>('content');
 const profile = computed(() => content?.value?.profile);
+const socials = computed(() => content?.value?.profile?.socials);
 </script>
