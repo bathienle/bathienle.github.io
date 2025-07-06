0606955083a155f3a58a3fa4ab037990b06f63c2
Ba Thien Le <bathien.le.pro@gmail.com>
2025-07-06 10:52:42 +0200

    Fix the width of image icon (#40)

--- src/components/TechIcon.vue
+++ src/components/TechIcon.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="max-w-sm shadow-lg bg-gray-600 p-2 rounded-xl">
-    <div class="h-12 w-full">
+    <div class="h-12 w-12">
       <a :href="tech.url" target="_blank" rel="noopener" class="relative group">
         <img class="object-contain w-full h-full" :alt="tech.name" :src="getImageSrc()" />
         <span class="absolute left-1/2 -translate-x-1/2 -top-10 px-2 py-1 rounded bg-gray-800 text-white text-xs opacity-0 group-hover:opacity-100 transition-opacity pointer-events-none whitespace-nowrap z-20 shadow-lg">
