da3b93975dcff1f8c71f330d50c1e377380fd04f
Ba Thien Le <bathien.le.pro@gmail.com>
2025-08-13 20:04:43 +0200

    Add test coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,6 +6,9 @@ export default mergeConfig(
   viteConfig,
   defineConfig({
     test: {
+      coverage: {
+        reporter: ['text'],
+      },
       environment: 'jsdom',
       exclude: [...configDefaults.exclude, 'e2e/**'],
       root: fileURLToPath(new URL('./', import.meta.url)),
