42e6eba11a4b2a6e132701abc4559db16e288b55
Ba Thien Le <bathien.le.pro@gmail.com>
2025-08-16 21:46:10 +0200

    Add hovering background on entry

--- src/components/ExperienceItem.vue
+++ src/components/ExperienceItem.vue
@@ -1,37 +1,28 @@
 <template>
-  <div class="flex items-center">
-    <div class="flex-none flex justify-center items-center w-20">
-      <div class="flex justify-center items-center w-16 h-16 bg-gray-600 rounded-lg">
-        <img :src="experience.icon" :alt="experience.company" />
+  <div class="p-2 rounded-lg hover:bg-gray-700">
+    <div class="flex items-center">
+      <div class="flex-none flex justify-center items-center w-20">
+        <div class="flex justify-center items-center w-16 h-16 bg-gray-600 rounded-lg">
+          <img :src="experience.icon" :alt="experience.company" />
+        </div>
       </div>
-    </div>
 
-    <div class="flex-1 flex justify-center items-center mx-4">
-      <h3 class="text-3xl text-center">{{ experience.title }}</h3>
-    </div>
+      <div class="flex-1 flex justify-center items-center">
+        <h3 class="text-3xl text-center">{{ experience.title }}</h3>
+      </div>
 
-    <div class="flex-none flex flex-col items-end gap-y-2">
-      <div class="text-2xl font-bold">{{ experience.company }}</div>
-      <time>{{ experience.startDate }} - {{ experience.endDate }}</time>
+      <div class="flex-none flex flex-col items-end gap-y-2 pr-2">
+        <div class="text-2xl font-bold">{{ experience.company }}</div>
+        <time>{{ experience.startDate }} - {{ experience.endDate }}</time>
+      </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { Icon } from '@iconify/vue';
-import { ref } from 'vue';
-
 import type { Experience } from '@/types/content.ts';
-
-import TechIcon from '@/components/TechIcon.vue';
 
 defineProps<{
   experience: Experience,
 }>();
-
-const collapsed = ref(true);
-
-const toggle = () => {
-  collapsed.value = !collapsed.value;
-};
 </script>
