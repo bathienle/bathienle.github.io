b19cb7a5d4ede79940a01a66cd86a1705d5f3f30
Ba Thien Le <bathien.le.pro@gmail.com>
2025-08-16 22:13:43 +0200

    Refactor experience item (#92)

--- src/components/ExperienceItem.vue
+++ src/components/ExperienceItem.vue
@@ -1,70 +1,48 @@
 <template>
-  <div class="relative pt-2 pb-4 pl-20">
-    <div
-      class="flex items-center justify-center w-8 h-8
-            lg:absolute lg:left-[5%] lg:w-[50px] lg:h-[50px]
-            lg:bg-gray-500 lg:rounded-lg"
-    >
-      <img :src="experience.icon" :alt="experience.company" />
+  <div
+    class="p-2 rounded-lg hover:bg-gray-700"
+    @click="isOpen = !isOpen"
+    @mouseenter="isOpen = true"
+    @mouseleave="isOpen = false"
+  >
+    <div class="flex items-center">
+      <div class="flex-none flex justify-center items-center w-20">
+        <div class="flex justify-center items-center w-16 h-16 bg-gray-600 rounded-lg">
+          <img :src="experience.icon" :alt="experience.company" />
+        </div>
+      </div>
+
+      <div class="flex-1 flex justify-center items-center">
+        <h3 class="text-3xl text-center">{{ experience.title }}</h3>
+      </div>
+
+      <div class="flex-none flex flex-col items-end gap-y-2 pr-2">
+        <div class="text-2xl font-bold">{{ experience.company }}</div>
+        <time>{{ experience.startDate }} - {{ experience.endDate }}</time>
+      </div>
     </div>
 
-    <div class="w-full flex-1 mt-2 ml-4 pl-15">
-      <div class="flex items-center justify-between cursor-pointer select-none" @click="toggle">
-        <div>
-          <h3 class="text-xl mb-1 text-[color:var(--color-heading)]">
-            {{ experience.title }}
-          </h3>
-          <span class="flex text-gray-500 p-1.5">
-            <span class="pr-2 text-3xl">
-              <Icon icon="mdi:calendar-clock-outline"/>
-            </span>
-            {{ experience.startDate }} - {{ experience.endDate }}
-          </span>
-        </div>
-        <button
-          class="ml-2 focus:outline-none"
-          :aria-label="collapsed ? 'Expand details' : 'Collapse details'"
-          tabindex="-1"
-          type="button"
-        >
-          <svg
-            :class="['transition-transform duration-200', collapsed ? 'rotate-0' : 'rotate-180']"
-            xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" viewBox="0 0 24 24">
-            <path stroke="currentColor" stroke-width="2" d="M6 9l6 6 6-6" />
-          </svg>
-        </button>
+    <div v-show="isOpen" class="text-gray-400 p-2">
+      <p class="flex-none flex items-center justify-end">
+        <span class="pr-2 text-3xl">
+          <Icon icon="mdi:location" />
+        </span>
+        {{ experience.location }}
+      </p>
+
+      <p class="text-justify p-1.5">
+        {{ experience.description }}
+      </p>
+
+      <ul class="list-disc pl-7.5 p-1.5">
+        <li v-for="(item, index) in experience.achievements" :key="index">
+          {{ item }}
+        </li>
+      </ul>
+
+      <div class="flex flex-wrap justify-center gap-4 pt-8">
+        <TechIcon v-for="tech in experience.stack" :key="tech.key" :tech="tech" />
       </div>
-      <transition name="fade">
-        <div v-show="!collapsed">
-          <p class="flex text-gray-500 p-1.5">
-            <span class="pr-2 text-3xl">
-              <Icon icon="mdi:location" />
-            </span>
-            {{ experience.location }}
-          </p>
-
-          <p class="flex text-gray-500 p-1.5">
-            <span class="pr-2 text-3xl">
-              <Icon icon="mdi-office-building"/>
-            </span>
-            {{ experience.company }}
-          </p>
-
-          <p class="text-justify text-gray-600 p-1.5">
-            {{ experience.description }}
-          </p>
-
-          <ul class="list-disc text-gray-600 pl-7.5 p-1.5">
-            <li v-for="(item, index) in experience.achievements" :key="index">
-              {{ item }}
-            </li>
-          </ul>
-
-          <div class="flex flex-wrap gap-4 pt-4">
-            <TechIcon v-for="tech in experience.stack" :key="tech.key" :tech="tech" />
-          </div>
-        </div>
-      </transition>
     </div>
   </div>
 </template>
@@ -73,17 +51,13 @@
 import { Icon } from '@iconify/vue';
 import { ref } from 'vue';
 
-import type { Experience } from '@/types/content.ts';
-
 import TechIcon from '@/components/TechIcon.vue';
+
+import type { Experience } from '@/types/content.ts';
 
 defineProps<{
   experience: Experience,
 }>();
 
-const collapsed = ref(true);
-
-const toggle = () => {
-  collapsed.value = !collapsed.value;
-};
+const isOpen = ref(false);
 </script>
--- src/views/ExperienceView.vue
+++ src/views/ExperienceView.vue
@@ -1,16 +1,15 @@
 <template>
-  <section id="experience" class="scroll-mt-20">
+  <section id="experience" class="scroll-mt-20 pt-10">
     <h1 class="text-3xl text-gray-300 font-bold p-1.5">Experience</h1>
 
     <div class="py-4">
       <hr class="w-full border-t-2 border-white mt-2">
     </div>
 
-    <ExperienceItem
-      v-for="experience in experiences"
-      :key="experience.company"
-      :experience="experience"
-    />
+    <div v-for="(experience, index) in experiences" :key="experience.company" class="m-2">
+      <ExperienceItem :experience="experience" />
+      <hr v-if="index < experiences.length - 1" class="border-t border-gray-600 my-4" />
+    </div>
   </section>
 </template>
 
@@ -23,5 +22,5 @@ import type { Content } from '@/types/content.ts';
 import ExperienceItem from '@/components/ExperienceItem.vue';
 
 const content = inject<Ref<Content>>('content');
-const experiences = computed(() => content?.value?.experiences);
+const experiences = computed(() => content?.value?.experiences ?? []);
 </script>
