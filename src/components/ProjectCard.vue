760acdd9ef305bfffc54f26ffb23bb90b79239e1
Ba Thien Le <bathien.le.pro@gmail.com>
2025-09-07 20:30:57 +0200

    Add project informations

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -1,11 +1,25 @@
 <template>
-  <div class="max-w-4xl w-full rounded-2xl overflow-hidden shadow-lg bg-secondary border border-neutral">
-    <div class="grid grid-cols-2 items-center gap-4">
-      <div>
-        <h3 class="text-2xl">{{ project.title }}</h3>
+  <div class="max-w-4xl w-full rounded-3xl overflow-hidden shadow-lg bg-secondary border border-neutral">
+    <div class="grid grid-cols-2 items-center gap-4 p-6">
+      <div class="flex flex-col justify-between h-full">
+        <div class="flex flex-col gap-2">
+          <h3 class="text-2xl">{{ project.title }}</h3>
+
+          <div class="flex items-center pb-4 gap-2">
+            <Icon class="text-2xl" icon="mdi:calendar-clock-outline" />
+            {{ project.startDate }} - {{ project.endDate }}
+          </div>
+        </div>
+
+        <p>{{ project.description }}</p>
+
+        <div class="flex flex-wrap gap-3">
+          <TechIcon v-for="tech in project.stack" :key="tech.key" :tech="tech"
+            class="transform hover:scale-110 transition-transform duration-200" />
+        </div>
       </div>
 
-      <div class="flex flex-col p-6 gap-5">
+      <div class="flex flex-col gap-5">
         <div class="w-full overflow-hidden rounded-2xl border border-neutral">
           <img class="object-contain w-full h-full" :src="project.image" :alt="project.title" />
         </div>
@@ -30,7 +44,6 @@
 <script setup lang="ts">
 import { Icon } from '@iconify/vue';
 
-import TagItem from '@/components/TagItem.vue';
 import TechIcon from '@/components/TechIcon.vue';
 import type { Project } from '@/types/content.ts';
 
