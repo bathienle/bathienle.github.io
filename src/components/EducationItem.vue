7cc434923ad8af63a6fc00a5918a4005b527b7c2
Ba Thien Le <bathien.le.pro@gmail.com>
2025-09-11 22:16:01 +0200

    Fix layout on mobile

--- src/components/EducationItem.vue
+++ src/components/EducationItem.vue
@@ -1,24 +1,24 @@
 <template>
   <div class="p-2 rounded-lg bg-secondary">
-    <div class="flex items-center">
-      <div class="flex-none flex justify-center items-center w-20">
+    <div class="flex flex-col md:flex-row items-center">
+      <div class="flex-none flex justify-center items-center w-20 mb-3 md:mb-0">
         <div class="flex justify-center items-center w-16 h-16 bg-neutral rounded-lg">
           <img :src="education.icon" :alt="education.institution" />
         </div>
       </div>
 
-      <div class="flex-1 flex justify-center items-center">
-        <h3 class="text-3xl text-center">{{ education.degree }}</h3>
+      <div class="flex-1 flex justify-center items-center mb-3 md:mb-0">
+        <h3 class="text-xl md:text-3xl text-center">{{ education.degree }}</h3>
       </div>
 
-      <div class="flex-none flex flex-col items-end gap-y-2 pr-2">
-        <div class="text-2xl font-bold">{{ education.institution }}</div>
-        <time>{{ education.startDate }} - {{ education.endDate }}</time>
+      <div class="flex-none flex flex-col items-center md:items-end gap-y-2 md:pr-2">
+        <div class="text-lg md:text-2xl font-bold text-center md:text-right">{{ education.institution }}</div>
+        <time class="text-sm md:text-base">{{ education.startDate }} - {{ education.endDate }}</time>
       </div>
     </div>
 
     <div class="p-2">
-      <p class="flex items-center justify-end">
+      <p class="flex items-center justify-center md:justify-end">
         <span class="pr-2 text-3xl">
           <Icon icon="mdi:location" />
         </span>
--- src/components/ExperienceItem.vue
+++ src/components/ExperienceItem.vue
@@ -4,25 +4,25 @@
     :class="{ 'bg-secondary': isOpen }"
     @click="isOpen = !isOpen"
   >
-    <div class="flex items-center">
-      <div class="flex-none flex justify-center items-center w-20">
+    <div class="flex flex-col md:flex-row items-center">
+      <div class="flex-none flex justify-center items-center w-20 mb-3 md:mb-0">
         <div class="flex justify-center items-center w-16 h-16 rounded-lg bg-neutral">
           <img :src="experience.icon" :alt="experience.company" />
         </div>
       </div>
 
-      <div class="flex-1 flex justify-center items-center">
+      <div class="flex-1 flex justify-center items-center mb-3 md:mb-0">
         <h3 class="text-3xl text-center">{{ experience.title }}</h3>
       </div>
 
-      <div class="flex-none flex flex-col items-end gap-y-2 pr-2">
+      <div class="flex-none flex flex-col items-end gap-y-2 md:pr-2">
         <div class="text-2xl font-bold">{{ experience.company }}</div>
         <time>{{ experience.startDate }} - {{ experience.endDate }}</time>
       </div>
     </div>
 
     <div v-show="isOpen" class="p-2">
-      <p class="flex-none flex items-center justify-end">
+      <p class="flex-none flex items-center justify-center md:justify-end">
         <span class="pr-2 text-3xl">
           <Icon icon="mdi:location" />
         </span>
