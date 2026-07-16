d94bfe98db2631468c43824ad012bdcbc057ce46
Ba Thien Le <bathien.le.pro@gmail.com>
2026-07-16 21:58:24 +0200

    Add 404 page not found

--- vite.config.ts
+++ vite.config.ts
@@ -23,4 +23,12 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
+  build: {
+    rollupOptions: {
+      input: {
+        main: fileURLToPath(new URL('./index.html', import.meta.url)),
+        notFound: fileURLToPath(new URL('./404.html', import.meta.url)),
+      },
+    },
+  },
 });
